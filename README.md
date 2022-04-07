02a4734643a9263ff4c474dd272f8c386d1e299a
default-sameer <sameerjoshicr7@gmail.com>
2022-04-07 09:42:14 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# NextJS Typescript Tailwind Starter Template 
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) typescript and tailwind.
+
+## Getting Started
+
+First get started by cloning the repo:
+```
+$ git clone https://github.com/default-sameer/nextjs-typescript-tailwind-starter-template.git
+```
+
+Run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+   <>
+      <div className="font-syne">
+        <h1 className="text-center text-2xl sm:text-5xl sm:pt-20 pt-10 font-bold">Hello from NextJs, Typescript, Tailwind and DaisyUi</h1>
+        <div className="flex flex-col container max-w-sm mx-auto sm:max-w-lg">
+          <p className="text-center p-5">Get Started by cloning the repo</p>
+          <div className="mockup-code">
+            <pre data-prefix="$"><code>git clone https://github.com/default-sameer/nextjs-typescript-tailwind-starter-template.git</code></pre>
+          </div>
+          <p className="text-center p-5">and editing <code>index.tsx</code></p>
+        </div>
+      </div>
+   </>
+  )
+}
+
+export default Home
